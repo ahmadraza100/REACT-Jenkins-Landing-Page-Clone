986153ed0f0c22194a10e2fa5eea889a89194c51
Ahmad <ahmadrazashafi@gmail.com>
2021-04-18 17:50:19 +0500

    Hosted

--- src/Components/CardSlider/CardData.js
+++ src/Components/CardSlider/CardData.js
@@ -3,43 +3,43 @@
         let Data=[
             {
                 title:"Transforming ",
-                disc:"Customers today expect highly reliable and up-to-date applications. As a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
+                disc:"Customers today expect highly reliable and up-to-date applications. Ahmad Raza 19738 a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
                 link:"https://www.redhat.com/en/events/webinar/transforming-test-driven-workflows-into-ci/cd-pipelines-with-jenkins"
 
             },
             {
                 title:"Transforming Test-Driven Workflows Into CI/CD Pipelines With Jenkins",
-                disc:"Customers today expect highly reliable and up-to-date applications. As a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
+                disc:"Customers today expect highly reliable and up-to-date applications. Ahmad Raza 19738 a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
                 link:"https://www.redhat.com/en/events/webinar/transforming-test-driven-workflows-into-ci/cd-pipelines-with-jenkins"
 
             },
             {
                 title:"Transforming Test-Driven Workflows Into CI/CD Pipelines With Jenkins",
-                disc:"Customers today expect highly reliable and up-to-date applications. As a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
+                disc:"Customers today expect highly reliable and up-to-date applications. Ahmad Raza 19738 a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
                 link:"https://www.redhat.com/en/events/webinar/transforming-test-driven-workflows-into-ci/cd-pipelines-with-jenkins"
 
             },
             {
                 title:"Transforming Test-Driven Workflows Into CI/CD Pipelines With Jenkins",
-                disc:"Customers today expect highly reliable and up-to-date applications. As a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
+                disc:"Customers today expect highly reliable and up-to-date applications. Ahmad Raza 19738 a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
                 link:"https://www.redhat.com/en/events/webinar/transforming-test-driven-workflows-into-ci/cd-pipelines-with-jenkins"
 
             },
             {
                 title:"Transforming Test-Driven Workflows Into CI/CD Pipelines With Jenkins",
-                disc:"Customers today expect highly reliable and up-to-date applications. As a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
+                disc:"Customers today expect highly reliable and up-to-date applications. Ahmad Raza 19738 a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
                 link:"https://www.redhat.com/en/events/webinar/transforming-test-driven-workflows-into-ci/cd-pipelines-with-jenkins"
 
             },
             {
                 title:"Transforming Test-Driven Workflows Into CI/CD Pipelines With Jenkins",
-                disc:"Customers today expect highly reliable and up-to-date applications. As a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
+                disc:"Customers today expect highly reliable and up-to-date applications. Ahmad Raza 19738 a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
                 link:"https://www.redhat.com/en/events/webinar/transforming-test-driven-workflows-into-ci/cd-pipelines-with-jenkins"
 
             },
             {
                 title:"Transforming Test-Driven Workflows Into CI/CD Pipelines With Jenkins",
-                disc:"Customers today expect highly reliable and up-to-date applications. As a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
+                disc:"Customers today expect highly reliable and up-to-date applications. Ahmad Raza 19738 a developer, you need to accelerate the application delivery process while maximizing the code quality to stay a step ahead of your competitors.",
                 link:"https://www.redhat.com/en/events/webinar/transforming-test-driven-workflows-into-ci/cd-pipelines-with-jenkins"
 
             }
--- src/Components/carsoul/carsoul.js
+++ src/Components/carsoul/carsoul.js
@@ -17,7 +17,7 @@ function Carsoul() {
           <img className="d-block w-100 " src={img} alt="First slide"/>
           <div class="carousel-caption d-none d-md-block">
     <h5>She Codes from Africa</h5>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s 
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Ahmad Raza 19738 standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s 
     </p>
   </div>
         </div>          
